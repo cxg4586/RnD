041dc6d4fc978520dac9b6326bbbb1bf74303cf9
cxg4586 <38442648+cxg4586@users.noreply.github.com>
2018-10-07 14:45:17 +1300

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+// File for Requirements for Connection
+$host = "cmslamp14.aut.ac.nz"; // Host Name
+$user = ""; // Username
+$pswd = ""; // Password
+$dbnm = "register"; // Database Name
+?>
