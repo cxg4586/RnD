33a036ce819425a62e157f83ac7faaca4e0b2ae9
cxg4586 <38442648+cxg4586@users.noreply.github.com>
2019-03-11 16:30:34 +1300

    Update profile.php

--- other file/profile.php
+++ other file/profile.php
@@ -3,66 +3,108 @@ require('settings.php');
 include("auth.php");
 ?>
 <!DOCTYPE html>
+<meta charset="UTF-8">
 <style>
-#wrapper div {
-	margin: 0 auto;
+.preview {
+    margin: 10px;
+    display: none;
 }
-#wrapper {
-	margin: 0 auto;
-	width: 100%;
-	text-align:center;
-	position: absolute;
-	top:160px;
+.preview--rounded {
+    width: 160px;
+    height: 160px;
+    border-radius: 50%;
 }
-img {
-	border-radius: 50%;
-	width: 150px;
-}
-#bottom {
-	bottom: 20px;
-}
-	
-#gallery .thumbnail{
-width:150px;
-height: 150px;
-float:left;
-margin:2px;
-}
-#gallery .thumbnail img{
-width:150px;
-height: 150px;
-}
-
 </style>
 
 <html>
 <head>
-<meta charset="utf-8">
+<meta charset="UTF-8">
 <title>Welcome <?php echo $_SESSION['username']; ?></title>
 <script src = "jquery-2.1.4.js"></script>
 <link rel="stylesheet" href="css/style.css" />
+
+<link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="assets/css/profile-picture.css">
+<meta name="viewport" content="width=device-width">
+
 </head>
 <body>
-<div class="form">
-<p>Welcome to your profile, <?php echo $_SESSION['username']; ?>!!!</p>
-<p>Good.</p>
+<div class="profile">
+        <div class="photo">
+            <input type="file" accept="image/*">
+            <div class="photo__helper">
+                <div class="photo__frame photo__frame--circle">
+                    <canvas class="photo__canvas"></canvas>
+                    <div class="message is-empty">
+                        <p class="message--desktop">Drop your photo here or browse your computer.</p>
+                        <p class="message--mobile">Tap here to select your picture.</p>
+                    </div>
+                    <div class="message is-loading">
+                        <i class="fa fa-2x fa-spin fa-spinner"></i>
+                    </div>
+                    <div class="message is-dragover">
+                        <i class="fa fa-2x fa-cloud-upload"></i>
+                        <p>Drop your photo</p>
+                    </div>
+                    <div class="message is-wrong-file-type">
+                        <p>Only images allowed.</p>
+                        <p class="message--desktop">Drop your photo here or browse your computer.</p>
+                        <p class="message--mobile">Tap here to select your picture.</p>
+                    </div>
+                    <div class="message is-wrong-image-size">
+                        <p>Your photo must be larger than 350px.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="photo__options hide">
+                <div class="photo__zoom">
+                    <input type="range" class="zoom-handler">
+                </div><a href="javascript:;" class="remove"><i class="fa fa-trash"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+    <button type="button" id="previewBtn">Preview</button>
+    <hr>
+    <img src="" alt="" class="preview">
+    <img src="" alt="" class="preview preview--rounded">
+
+    <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="vendor/jquery/jquery.min.js"><\/script>')
+    </script>
+    <script src="scripts/profile-picture.js"></script>    
+    <script>
+    /**
+     * DEMO
+     */ 
+    var pp = new profilePicture('.profile', 'Unknown Person.png',
+        {
+            onLoad: function (type) {
+                $('.preview').hide().attr('src','');
+            },
+            onChange: printOutput,
+            onRemove: function (type) {
+                $('.preview').hide().attr('src','');
+            },
+            onError: function (type) {
+                console.log('Error type: ' + type);
+            }
+        });
+
+    $('#previewBtn').on('click', function() {
+        $('.preview').show().attr('src',pp.getAsDataURL());  
+    });
+
+    function printOutput(data) {
+        //console.log(data);
+    }
+    </script>
 
 <p><a href="Home.html">Home</a></p>
 <a href="logout.php">Logout</a>
-</div>
-
-<div id = "wrapper">
-	<input type="file" id="fileinput" multiple="multiple" accept="image/*" />
-
-	<div id="gallery"></div>
-	<script src="gallery.js"></script>
-	
-	<img src = "Unknown Person.png" />
-	<div id = "bottom">
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $_SESSION['username']; ?>
-	<br/><br/>
-	</div>
-</div>
 
 </body>
 </html>
