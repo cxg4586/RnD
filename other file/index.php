d39a2585f85c152ab2cf2b3cf2a9897bb9bcc974
hmv7201 <37851346+hmv7201@users.noreply.github.com>
2019-04-01 22:51:48 +1300

    Update index.php
    Commented out line 57 and 102.
    Changed all single and double quotes.

--- other file/index.php
+++ other file/index.php
@@ -1,4 +1,4 @@
-<?php include (“settings.php”);?>
+<?php include ("settings.php");?>
 
 <!doctype html>
 
@@ -6,43 +6,43 @@
 
 <head>
 
-<meta charset=”utf-8″>
+<meta charset="utf-8″>
 
 <title>Comment to Post</title>
 
-<link rel=”stylesheet” type=”text/css” href=”styles.css”>
+<link rel="stylesheet" type="text/css" href="styles.css">
 
 </head>
 
 <body>
 
-<div class=”body”>
+<div class="body">
 
 Simple Posting Updates With Comment in PHP and MYSQL
 
 </div>
 
-<div class=”body”>
+<div class="body">
 
-<form method=”post” action=”post-action.php”>
+<form method="post" action="post-action.php">
 
 <label>Add Post:</label><br>
 
-<textarea name=”post_msg” required></textarea><br>
+<textarea name="post_msg" required></textarea><br>
 
-<input type=”text” name=”uname” placeholder=”type your name here…” required /><br>
+<input type="text" name="uname" placeholder="type your name here…" required /><br>
 
-<input type=”submit” name=”post” value=”Post” />
+<input type="submit" name="post" value="Post" />
 
 </form>
 
 <?php
 
-if (isset($_GET[‘post_action’])) {
+if (isset($_GET['post_action'])) {
 
-if ($_GET[‘post_action’] == “posted”) {
+if ($_GET['post_action'] == "posted") {
 
-echo ‘Successfully Posted!’;
+echo 'Successfully Posted!';
 
 }
 
@@ -54,51 +54,59 @@ echo ‘Successfully Posted!’;
 
 <?php
 
-$posts = $mysqli->query(“SELECT * FROM posts ORDER BY post_id DESC”);
+//$posts = $mysqli->query("SELECT * FROM posts ORDER BY post_id DESC");
+
+if(!$con){
+	die('Could not connect to database.'.mysql_error());
+}else{
+	$post_query="SELECT * FROM posts ORDER BY post_id DESC";
+	$posts=mysqli_query($con,$post_query);
+}
+
 
 ?>
 
-<div class=”body”>
+<div class="body">
 
 <b><?php echo $posts->num_rows ?></b> total posts
 
 </div>
 
-<div class=”body” style=”padding-bottom: 1px;”>
+<div class="body" style="padding-bottom: 1px;">
 
 <?php
 
 if ($posts->num_rows == null) {
 
-echo ‘no posts yet’;
+echo 'no posts yet';
 
 } else if ($posts->num_rows != null) {
 
 while ($post_data = $posts->fetch_assoc()) { ?>
 
-<div class=”post-panel”>
+<div class="post-panel">
 
-<div class=”post-header”>
+<div class="post-header">
 
-<b><?php echo $post_data[‘post_name’] ?></b>
+<b><?php echo $post_data['post_name'] ?></b>
 
 </div>
 
-<div class=”post-body”>
+<div class="post-body">
 
-<?php echo $post_data[‘post_msg’] ?>
+<?php echo $post_data['post_msg'] ?>
 
 </div>
 
 <?php
-
-$comments = $mysqli->query(“SELECT * FROM comments WHERE post_id = “.$post_data[‘post_id’].””);
-
+//$comments = $mysqli->query("SELECT * FROM comments WHERE post_id = ".$post_data['post_id']."");
+$comments="SELECT * FROM comments WHERE post_id = ".$post_data['post_id']."";
+$comments=mysqli_query($con,$comments);
 ?>
 
-<div class=”post-footer”>
+<div class="post-footer">
 
-<a href=”view-post.php?post_id=<?php echo $post_data[‘post_id’] ?>”>Comment (<?php echo $comments->num_rows ?>)</a>
+<a href="view-post.php?post_id=<?php echo $post_data['post_id'] ?>">Comment (<?php echo $comments->num_rows ?>)</a>
 
 </div>
 
