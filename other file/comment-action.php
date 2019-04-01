edbf1fcd3b9733a054a4705a8ff3400f4c1e7b47
hmv7201 <37851346+hmv7201@users.noreply.github.com>
2019-04-01 22:45:01 +1300

    Update comment-action.php
    I commented out line 13 and changed it to lines 14 and 15

--- other file/comment-action.php
+++ other file/comment-action.php
@@ -1,20 +1,22 @@
 <?php
 
-include ‘settings.php’;
+include 'settings.php';
 
-if (isset($_POST[‘post_comment’])) {
+if (isset($_POST['post_comment'])) {
 
-$post_id = $_GET[‘post_id’];
+$post_id = $_GET['post_id'];
 
-$comment = $_POST[‘comment’];
+$comment = $_POST['comment'];
 
-$uname = $_POST[‘uname’];
+$uname = $_POST['uname'];
 
-$comment = $mysqli->query(“INSERT INTO comments (post_id, user_name, user_comment) VALUES ($post_id, ‘$uname’, ‘$comment’)”);
+//$comment = $mysqli->query(“INSERT INTO comments (post_id, user_name, user_comment) VALUES ($post_id, '$uname', '$comment')”);
+$comment="INSERT INTO comments (post_id, user_name, user_comment) VALUES ($post_id, '$uname', '$comment')";
+$comment=mysqli_query($con,$comment);
 
 if ($comment) {
 
-header(“Location: view-post.php?post_id=$post_id”);
+header("Location: view-post.php?post_id=$post_id");
 
 } else {
 
