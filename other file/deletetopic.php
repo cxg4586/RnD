d2705e1289f4fa88a1a73410c5e5961b976e3bb8
hmv7201 <37851346+hmv7201@users.noreply.github.com>
2019-04-29 01:19:20 +1200

    Create deletetopic.php

---
+++ other file/deletetopic.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once "settings.php";
+session_start();
+$username = $_SESSION['username'] ?? false;
+$isadmin = $_SESSION['isadmin'] ?? false;
+
+if(!$username){
+    header("Location: login.php");
+    exit();
+}
+//check for required fields from the form
+if ( (!$_POST['topic_title'])
+    || (!$_POST['post_text'])) {
+    header("Location: addtopic.html");
+    exit;
+}
+
+//create and issue the second query
+$createAnotherQueryTable = "CREATE TABLE IF NOT EXISTS `forum_posts` (
+  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_title` varchar(191) DEFAULT NULL,
+  `post_text` text,
+  `post_create_time` datetime DEFAULT NULL,
+  `post_owner` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`post_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;";
+
+mysqli_query($con, $createAnotherQueryTable);
+$text = mysqli_real_escape_string($con, $_POST['post_text']);
+$add_post = "insert into forum_posts(post_title, post_text, post_create_time, post_owner) values ('".$_POST['topic_title']."',
+'$text', now(), '$username')";
+mysqli_query($con, $add_post) or die(mysqli_error($con));
+
+header('Location: topic.php?id='.$con->insert_id);
+
+//create nice message for user
+$msg = "<P>The <strong>$_POST[topic_title]</strong> topic has been created.</p>";
+?>
+<html>
+<head>
+    <title>New Topic Added</title>
+</head>
+<body>
+<h1>New Topic Added</h1>
+<?php print $msg; ?>
+</body>
+</html>
