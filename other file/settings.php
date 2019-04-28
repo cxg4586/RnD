f0b6c2aef87132e3a47d381af835c85595c5c008
hmv7201 <37851346+hmv7201@users.noreply.github.com>
2019-04-29 01:28:39 +1200

    Update settings.php

--- other file/settings.php
+++ other file/settings.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
 $con = mysqli_connect("localhost","","","");
